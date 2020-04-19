6352fe55d435cc41a2a72fbcf03ec1a5b87caa56
buckblack <42344854+buckblack@users.noreply.github.com>
2020-04-19 13:48:32 +0700

    17

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/src/assets'));
+app.use(express.static(__dirname + '/'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/src/assets/index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 // default Heroku PORT
